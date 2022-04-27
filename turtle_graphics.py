84131bef0dbc8b4b273d1ce4628a9e0d580b60a6
mglazik <104222232+mglazik@users.noreply.github.com>
2022-04-27 14:21:47 +0200

    Add files via upload

--- turtle_graphics.py
+++ turtle_graphics.py
@@ -1,23 +1,30 @@
 #!/bin/python
 # -*- coding: utf-8 -*-
 
+try:
+        l = int(input("Podaj kąt "))
 
-# dlaczego nie rysuje?
+        if l < 0:
+                print('W prawo nie skręcamy!')
 
-import turtle
+                
+        else:
+                import turtle
 
-screen = turtle.Screen()
-screen.setup(600, 600)
+                screen = turtle.Screen()
+                screen.setup(1800, 900)
 
 
-colors = ['red', 'purple', 'green', 'orange', "blue", 'yellow', ]
-t = turtle.Pen()
+                colors = ['red', 'purple', 'green', 'orange', "blue", 'yellow', ]
+                t = turtle.Pen()
 
-turtle.bgcolor("black")
+                turtle.bgcolor("black")
 
-for x in range(360):
-        t.pencolor(colors[x%6])
-        t.width(x/100 + 1)
-        t.forward(x*2+150+x/50)
-        t.left(590)
-        t.speed("fastest")
+                for x in range(360):
+                        t.pencolor(colors[x%6])
+                        t.width(x/100 + 0.5)
+                        t.forward(x)
+                        t.left(l)
+                        t.speed("fastest")
+except ValueError:
+        print('Ej, no weź się nie wygłupiaj i podaj liczbę')
