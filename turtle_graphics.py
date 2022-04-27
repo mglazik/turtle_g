9d65d2d4c43057a56df8c65965aeadb0d782b1d8
mglazik <104222232+mglazik@users.noreply.github.com>
2022-04-27 15:32:28 +0200

    Add files via upload

--- turtle_graphics.py
+++ turtle_graphics.py
@@ -4,32 +4,29 @@
 import sys
 
 try:
-        l = float(input("Podaj kąt "))
+        l = float(input("Podaj kąt: "))
+        s = float(input ('Podaj skok: '))
 
 except ValueError:
         print('Ej, no weź się nie wygłupiaj i podaj liczbę')
         sys.exit()
 
-if l < 0:
-        print('W prawo nie skręcamy!')
-        sys.exit()
-        
-else:
-        import turtle
 
-        screen = turtle.Screen()
-        screen.setup(1800, 900)
+import turtle
+
+screen = turtle.Screen()
+screen.setup(1800, 900)
 
 
-        colors = ['red', 'purple', 'green', 'orange', "blue", 'yellow', ]
-        t = turtle.Pen()
+colors = ['red', 'purple', 'green', 'orange', "blue", 'yellow', ]
+t = turtle.Pen()
 
-        turtle.bgcolor("black")
+turtle.bgcolor("black")
 
-        for x in range(1360):
-                t.pencolor(colors[x%6])
-                t.width(x/100 + 0.5)
-                t.forward(x*2+1)
-                t.left(l)
-                t.speed("fastest")
+for x in range(1360):
+        t.pencolor(colors[x%6])
+        t.width(x/100 + 0.5)
+        t.forward(x*2+s)
+        t.left(l)
+        t.speed("fastest")
 
